f85e813d8bfd893f8168bca185d8b678e7886006
IgorD2109 <igor.dias.pereira.2109@gmail.com>
2026-05-16 00:26:55 -0300

    Beecrowd

---
+++ 1013.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int a, b, c, MAIORAB;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+
+    MAIORAB = (a + b + abs * (a - b)) / 2;
+
+    printf("%d eh o maior", MAIORAB);
+
+    return 0;
+}
