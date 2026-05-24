8e734b8754a0557a1daff3917a03b408b5c0e436
IgorD2109 <igor.dias.pereira.2109@gmail.com>
2026-05-24 12:37:18 -0300

    1

---
+++ 1059.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+
+    int i;
+    i = 2;
+
+     while (i<=100) {   //varredura de 1 até 100
+       printf("%d\n", i);
+       i= i + 2;   
+   }
+
+    return 0;
+}
